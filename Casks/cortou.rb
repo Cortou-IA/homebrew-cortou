5351df780414c8e6ad434ccf01b2ddf9c925b559
Vinicius Valim <adm@dualeads.com>
2026-09-04 19:08:21 -0300

    cortou 1.0.7 (beta mac 2) - fix catalogo IA + nav configuracoes

---
+++ Casks/cortou.rb
@@ -0,0 +1,11 @@
+cask "cortou" do
+  version "1.0.7"
+  sha256 "ccfff65654f1e84a706fedf979818ce18a8b4fa5e8510cb61a43848a3fe39e81"
+
+  url "https://github.com/Cortou-IA/cortou-releases/releases/download/v#{version}-mac-beta2/Cortou-mac.zip"
+  name "Cortou"
+  desc "Editor automático de cortes pra TikTok/Reels/Shorts"
+  homepage "https://ocortou.com.br"
+
+  app "Cortou.app"
+end
